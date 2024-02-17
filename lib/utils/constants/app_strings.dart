38d10ee8a286279ef68edf95f16b8f962da83be0
Mohamed Abo-ghazalla <45886535+Abo-ghazalla@users.noreply.github.com>
2024-02-17 12:26:29 +0200

    create abstract AppStrings

---
+++ lib/utils/constants/app_strings.dart
@@ -0,0 +1,9 @@
+abstract class AppStrings {
+  static const currencyConverter = "Currency Converter";
+  static const tryAgain = "Try Again";
+  static const history = "History";
+  static const historyText = "Historical data for US dollar & Japanese yen for the last 7 days";
+  static const from = "From";
+  static const to = "To";
+  static const amount = "Amount";
+}
