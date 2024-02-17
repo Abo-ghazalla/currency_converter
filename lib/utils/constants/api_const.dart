2ad272ecbc6489b437f9eb3016b3c83169195681
Mohamed Abo-ghazalla <45886535+Abo-ghazalla@users.noreply.github.com>
2024-02-17 12:26:07 +0200

    create  abstract ApiConst

---
+++ lib/utils/constants/api_const.dart
@@ -0,0 +1,8 @@
+abstract class ApiConst {
+  static const baseUrl = "https://api.freecurrencyapi.com/v1/";
+  static const currencies="currencies"; 
+  static const latest="latest"; 
+  static const historical="historical"; 
+
+  
+}
